f542df3f637b8d9fd9f0d5849051be0ee6a70004
NikolasSilvaLunkes <nikolaslunkes@gmail.com>
2023-02-19 16:16:52 -0300

    well, not using yew anymore for a while

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use yew::prelude::*;
+
+
+#[derive(Clone, PartialEq)]
+struct Video {
+    id: usize,
+    title: String,
+    speaker: String,
+    url: String,
+}
+
+#[derive(Properties, PartialEq)]
+struct VideosListProps {
+    videos: Vec<Video>,
+}
+
+#[function_component(VideosList)]
+fn videos_list(VideosListProps { videos }: &VideosListProps) -> Html {
+    videos.iter().map(|video| html! {
+        <p key={video.id}>{format!("{}: {}", video.speaker, video.title)}</p>
+    }).collect()
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <>
+            <h1>{ "RustConf Explorer" }</h1>
+            <div>
+                <h3>{"Videos to watch"}</h3>
+               <VideosList videos={videos} />
+            </div>
+            // ...
+        </>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
